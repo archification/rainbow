e2fbc98b9854bd00bcb318f5ad40eed037b486a6
archification <archification@gmail.com>
2020-02-04 14:48:51 -0600

    fixed double print with blank lines

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,16 @@ fn stuff() {
         let mut rng = rand::thread_rng();
         let (r, g, b): (u8, u8, u8) = rng.gen();
         let rgb = termion::color::Rgb(r, g, b);
+        let (x, y, z): (u8, u8, u8) = rng.gen();
+        let xyz = termion::color::Rgb(x, y, z);
         println!(
-            "{}{}  guacamolification  {}{}",
+            "{}{}{}  guacamolification  {}{}{}",
             color::Fg(rgb),
+            color::Bg(xyz),
             rng.gen::<i64>(),
             rng.gen::<i64>(),
-            color::Fg(color::Reset)
-        );
-        println!("{}",
-            color::Bg(rgb)
+            color::Fg(color::Reset),
+            color::Bg(xyz)
         );
     }
 }
